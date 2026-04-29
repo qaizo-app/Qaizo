3e764c8408d00c49024b243d0ec5c64ffaf83e46
Qaizo <qaizo.app@gmail.com>
2026-04-29 15:04:43 +0300

    fix(ios): switch to use_frameworks! :linkage => :static for Firebase+gRPC

--- plugins/withModularHeaders.js
+++ plugins/withModularHeaders.js
@@ -2,19 +2,27 @@ const { withDangerousMod } = require('@expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
+// Firebase + gRPC require static frameworks on iOS.
+// use_modular_headers! alone breaks gRPC-Core.modulemap lookup.
 module.exports = function withModularHeaders(config) {
   return withDangerousMod(config, [
     'ios',
     async (config) => {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       let podfile = fs.readFileSync(podfilePath, 'utf8');
-      if (!podfile.includes('use_modular_headers!')) {
+
+      // Remove any previously injected use_modular_headers!
+      podfile = podfile.replace(/\nuse_modular_headers!/g, '');
+
+      // Static frameworks: fixes Firebase Swift pods AND gRPC module maps
+      if (!podfile.includes('use_frameworks!')) {
         podfile = podfile.replace(
           /^(platform :ios.+)$/m,
-          '$1\nuse_modular_headers!'
+          '$1\nuse_frameworks! :linkage => :static'
         );
-        fs.writeFileSync(podfilePath, podfile);
       }
+
+      fs.writeFileSync(podfilePath, podfile);
       return config;
     },
   ]);
